68eff82b802227f848067a6b2b2e5ac934ff3b4a
Toxiiic <872435708@qq.com>
2017-10-16 14:36:45 +0800

    add simple database trial code

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const Koa = require('koa');
 const bodyparser = require('koa-bodyparser');
 const controller = require('./controller');
 const staticFiles = require('./static-files');
+const Sequelize = require('sequelize');
+const config = require('./config');
+
 
 const app = new Koa();
 
@@ -16,6 +19,37 @@ app.use(async (ctx, next) => {
 app.use(bodyparser());
 app.use(controller());
 app.use(staticFiles('/static/', __dirname + '/pages'));
+
+
+var sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 30000
+    }
+});
+
+var User = sequelize.define('user', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+    name: Sequelize.STRING(20),
+    pwd: Sequelize.STRING(20),
+    head: Sequelize.STRING(50)
+}, {
+    timestamps: false
+});
+
+(async () => {
+    var user = await User.create({
+        name: '哦耶这是我的名字',
+        pwd: '123456'
+    });
+    console.log(user);
+})()
 
 
 app.listen(3000);
