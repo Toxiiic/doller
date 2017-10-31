cacba752a8cc717c3038475d8b1a6a3dd77f018c
Toxiiic <872435708@qq.com>
2017-10-31 15:13:02 +0800

    fix: modify session max age to 180 days

--- app.js
+++ app.js
@@ -23,22 +23,13 @@ const CONFIG = {
  /** (number || 'session') maxAge in ms (default is 1 days) */
  /** 'session' will result in a cookie that expires when session/browser is closed */
  /** Warning: If a session cookie is stolen, this cookie will never expire */
- maxAge: 86400000,
+ maxAge: 86400000 * 180,
  overwrite: true, /** (boolean) can overwrite or not (default true) */
  httpOnly: true, /** (boolean) httpOnly or not (default true) */
  signed: true, /** (boolean) signed or not (default true) */
  rolling: false, /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. default is false **/
 };
 app.use(session(CONFIG, app));
-// app.use(ctx => {
-//     // ignore favicon 
-//     if (ctx.path === '/favicon.ico') return;
-   
-//     let n = ctx.session.views || 0;
-//     ctx.session.views = ++n;
-//     ctx.session.signed = true;
-//     ctx.body = ctx.session.signed;
-// });
 
 app.use(rest.restify());
 /** 中间件的注册顺序很重要！！*/
